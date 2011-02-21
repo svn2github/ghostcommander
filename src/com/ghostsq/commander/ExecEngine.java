a5a3b38ea8228517b0a979a49841fa465112289f
zc2 <zc2@a2e631ca-c9e0-48dd-9a95-0888a2d8d389>
2011-02-21 16:45:56 +0000

    git-svn-id: svn://svn.code.sf.net/p/ghostcommander/code@108 a2e631ca-c9e0-48dd-9a95-0888a2d8d389

--- src/com/ghostsq/commander/ExecEngine.java
+++ src/com/ghostsq/commander/ExecEngine.java
@@ -11,7 +11,7 @@ import android.util.Log;
 
 class ExecEngine extends Engine {
     public final static String TAG = "ExecEngine";
-    public  String sh = "sh"; // DEBUG ONLY!!!!!!!!! don't forget to  change back to "su"!!!!!!!!!
+    public  String sh = "su";
     private Context conetxt;
     private String  where, command;
     private boolean use_busybox = false;
