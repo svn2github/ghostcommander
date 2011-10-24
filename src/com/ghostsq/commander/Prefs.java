a3d9081fe4109225a07d3b0dc4849adc90256208
zc2 <zc2@a2e631ca-c9e0-48dd-9a95-0888a2d8d389>
2011-10-24 02:55:15 +0000

    git-svn-id: svn://svn.code.sf.net/p/ghostcommander/code@214 a2e631ca-c9e0-48dd-9a95-0888a2d8d389

--- src/com/ghostsq/commander/Prefs.java
+++ src/com/ghostsq/commander/Prefs.java
@@ -1,8 +1,10 @@
 package com.ghostsq.commander;
 
+import com.ghostsq.commander.toolbuttons.ToolButtonsProps;
 import com.ghostsq.commander.utils.Utils;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -15,7 +17,8 @@ public class Prefs extends PreferenceActivity implements Preference.OnPreference
     public static final String BGR_COLORS = "bgr_color_picker"; 
     public static final String FGR_COLORS = "fgr_color_picker"; 
     public static final String SEL_COLORS = "sel_color_picker"; 
-    public static final String TTL_COLORS = "ttl_color_picker"; 
+    public static final String TTL_COLORS = "ttl_color_picker";
+    public static final String TOOLBUTTONS = "toolbar_preference";
     private SharedPreferences color_pref = null;
     private String color_pref_key = null;
     
@@ -39,15 +42,32 @@ public class Prefs extends PreferenceActivity implements Preference.OnPreference
         color_picker_pref = (Preference)findPreference( TTL_COLORS );
         if( color_picker_pref != null )
             color_picker_pref.setOnPreferenceClickListener( this );
+
+        Preference tool_buttons_pref;
+        tool_buttons_pref = (Preference)findPreference( TOOLBUTTONS );
+        if( tool_buttons_pref != null )
+            tool_buttons_pref.setOnPreferenceClickListener( this );
     }
 
 
     @Override
     public boolean onPreferenceClick( Preference preference ) {
-        color_pref = getSharedPreferences( COLORS_PREFS, Activity.MODE_PRIVATE );
-        color_pref_key = preference.getKey();
-        int color = color_pref.getInt( color_pref_key, getDefaultColor( color_pref_key ) );
-        new RGBPickerDialog( this, this, color ).show();
+        try {
+            color_pref_key = preference.getKey();
+            if( TOOLBUTTONS.equals( color_pref_key ) ) {
+                Intent intent = new Intent( Intent.ACTION_MAIN );
+                intent.setClass( this, ToolButtonsProps.class );
+                startActivity( intent );
+            }
+            else {
+                color_pref = getSharedPreferences( COLORS_PREFS, Activity.MODE_PRIVATE );
+                int color = color_pref.getInt( color_pref_key, getDefaultColor( color_pref_key ) );
+                new RGBPickerDialog( this, this, color ).show();
+            }
+            return true;
+        } catch( Exception e ) {
+            e.printStackTrace();
+        }
         return false;
     }
 
