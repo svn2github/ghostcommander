7ea6869d181db891374f79df247755a6d61132ea
zc2 <zc2@a2e631ca-c9e0-48dd-9a95-0888a2d8d389>
2012-02-13 02:44:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/ghostcommander/code@313 a2e631ca-c9e0-48dd-9a95-0888a2d8d389

--- src/com/ghostsq/commander/favorites/FavDialog.java
+++ src/com/ghostsq/commander/favorites/FavDialog.java
@@ -105,12 +105,17 @@ public class FavDialog implements OnClickListener {
             try {
                 f.setComment( ce.getText().toString() );
                 String path = pe.getText().toString().trim();
-                String serv = se.getText().toString().trim();
-                f.setUri( uri.buildUpon().encodedAuthority( Utils.encodeToAuthority( serv ) ).path( path ).build() );
-                Log.i( TAG, "Uri:" + f.getUri() );
-                String domain = de != null ? de.getText().toString().trim() : "";
-                String usernm = ue.getText().toString().trim();
-                f.setCredentials( domain.length() > 0 ? domain + ";" + usernm : usernm, we.getText().toString().trim() );
+                if( se != null ) {
+                    String serv = se.getText().toString().trim();
+                    f.setUri( uri.buildUpon().encodedAuthority( Utils.encodeToAuthority( serv ) ).path( path ).build() );
+                    Log.i( TAG, "Uri:" + f.getUri() );
+                    String domain = de != null ? de.getText().toString().trim() : "";
+                    String usernm = ue.getText().toString().trim();
+                    f.setCredentials( domain.length() > 0 ? domain + ";" + usernm : usernm, we.getText().toString().trim() );
+                }
+                else {
+                    f.setUri( uri.buildUpon().path( path ).build() );
+                }
                 owner.invalidate();
             } catch( Exception e ) {
                 Log.e( TAG, null, e );
