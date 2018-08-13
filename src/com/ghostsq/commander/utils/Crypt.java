ac2cb1d510096bbad3ed9343fa936b9e60f715c4
zc2 <zc2@a2e631ca-c9e0-48dd-9a95-0888a2d8d389>
2018-08-13 01:11:44 +0000

    git-svn-id: svn://svn.code.sf.net/p/ghostcommander/code@625 a2e631ca-c9e0-48dd-9a95-0888a2d8d389

--- src/com/ghostsq/commander/utils/Crypt.java
+++ src/com/ghostsq/commander/utils/Crypt.java
@@ -23,7 +23,7 @@ public class Crypt {
     }
     private static byte[] getRawKey( String seed ) throws Exception {
         KeyGenerator kgen = KeyGenerator.getInstance( "AES" );
-        SecureRandom sr = SecureRandom.getInstance( "SHA1PRNG", "Crypto" );
+        SecureRandom sr = SecureRandom.getInstance( "SHA1PRNG" );
         sr.setSeed( seed.getBytes() );
         kgen.init( 128, sr ); // 192 and 256 bits may not be available
         SecretKey skey = kgen.generateKey();
